642a4c7cb77015bbb1a2674b6ec95bd15f3b3dad
komodoman123 <rifqi546@gmail.com>
2016-11-08 13:38:25 +0700

    Add files via upload

---
+++ ControllerPocong.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;public class controller : MonoBehaviour
+{
+
+	private Vector2 jumpForce = new Vector2(0, 300);
+
+	private double jumpCount=1; 
+	public Rigidbody2D rb;
+	public bool isDead;
+	public Animator animator;
+
+	void Start(){	
+		rb = GetComponent<Rigidbody2D>();
+		PlayerPrefs.SetInt("speed", 5);
+		animator = new Animator ();
+	}
+	void Update ()
+	{
+
+
+		transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+
+		if (Input.GetKeyUp("space")&&jumpCount>0)
+		{
+
+		Jump ();
+
+		}
+
+		if(Input.GetKeyUp(KeyCode.DownArrow)){
+			Duck();
+		}
+
+
+	}
+	void Jump(){
+		
+		jumpCount = 0;
+	
+		GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		GetComponent<Rigidbody2D> ().AddForce (jumpForce);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		
+		jumpCount = 1;
+		if (coll.gameObject.tag == "obstacle") {
+			isDead = true;
+		} else {
+			isDead = false;
+		}
+	}
+
+
+	void Duck(){
+		
+			
+
+		GetComponent<Animator> ().Play ("sleding");
+		asd = true;
+	}}
+
+
+
+
