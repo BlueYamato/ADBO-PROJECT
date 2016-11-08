c471d951514288fd0e61613da349b749debe9b0a
ladiesman2177 <teditri38@gmail.com>
2016-11-08 14:12:37 +0700

    Add files via upload
    background update script

---
+++ bgscroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgscroll : MonoBehaviour {
+	public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, 0);
+		GetComponent<Renderer> ().material.mainTextureOffset = offset;
+		transform.Translate(Vector3.right * PlayerPrefs.GetInt("speed") * Time.deltaTime);
+	}
+}
